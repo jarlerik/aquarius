e9ec38db811fd39b6859f3f45d8d87f237e24e29
Jarl-Erik Malmström <jarl-erik.malmstrom@soikea.com>
2017-03-24 19:40:40 +0200

    Added couple of routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const Home = () => {
+  return (
+    <div>Home</div>
+  );
+};
 
-   componentDidMount() {
-    window.fetch('api/posts')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error))
-  }
+const About = () => {
+  return (
+    <div>About</div>
+  );
+};
+
+class App extends Component {
 
   render() {
     return (
@@ -18,9 +28,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Aquarius React client</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Router>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+
+              <hr/>
+
+              <Route exact path="/" component={Home}/>
+              <Route path="/about" component={About}/>
+            </div>
+          </Router>
       </div>
     );
   }
