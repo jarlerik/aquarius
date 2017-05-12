b582953b35e02431c1e7672746447e2b1c1253fd
Jarl-Erik Malmström <jarl-erik.malmstrom@soikea.com>
2017-05-12 14:48:42 +0300

    change

--- client/src/data/news.js
+++ client/src/data/news.js
@@ -3,8 +3,7 @@ export default {
 		header: "Aquarius Sporting Club",
 
 		p1: `Meillä voit kuntoilla valoisissa tiloissa, joista avautuu upea
-			merinäköala. Käytettävissäsi ovat Technogymin laitteet, mm. Selection Line sekä Kinesis, missä voit treenata
-			lihasvoimaa, tasapainoa sekä liikkuvuutta.`,
+			merinäköala. Käytettävissäsi ovat Technogymin laitteet, mm. Selection Line sekä Kinesis.`,
 
 		p2: `Olet myös tervetullut kokeilemaan erilaisia Les Millsin tunteja
 			sekä meidän muita jumppia. Kuntoilun jälkeen voit rentoutua viihtyisässä saunassa.`
@@ -14,8 +13,7 @@ export default {
 
 		p1: `Hos oss kan du träna i ljusa och rymliga utrymmen med en
 			fantastisk havsutsikt! Vi har utrustning från
-			Technogym, bl.a. Selection Line och Kinesis där du kan träna
-			muskelstyrka, balans och vighet.`,
+			Technogym, bl.a. Selection Line och Kinesis.`,
 
 		p2: `Du är även välkommen och pröva olika LesMills timmar och
 			våra andra jumppor. Efter träningen kan du koppla av i våra trivsamma bastun.`
