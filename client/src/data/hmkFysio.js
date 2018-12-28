2a7f56ceae294adf7d3362532237c9c97e5be4af
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2018-12-28 18:34:42 +0200

    Changed content data.

--- client/src/data/hmkFysio.js
+++ client/src/data/hmkFysio.js
@@ -7,20 +7,20 @@ export default {
         h2_3: "Koulutettu hieroja",
         h3_1: "Hanna Kanerva p. 0503647583",
         h3_2: "hanna.kanerva[at]outlook.com",
-        h2_4: "Fysiotraining",
-        p_1: "1 x 60min 65€",
-        p_2: "3 x 60min 180€",
-        p_3: "Henkilökohtainen harjoitusohjelma 65€ Fysiotraining paketin ostajille Inbody Aquariuksessa 30€."
+        h2_4: "Personal trainer",
+        p_1: "1 x 60min 80€ (sis. 24% alv)",
+        p_2: "3 x 60min 220€ (sis. 24% alv)",
+        p_3: "Pt paketin ostajille Inbody Aquariuksessa 30€"
     },
     sv: {
         h2_1: "FISAF Personal Trainer",
         h2_2: "Fysioterapeut YH",
-        h2_3: "Koulutettu hieroja",
+        h2_3: "Utbildad massör",
         h3_1: "Hanna Kanerva p. 0503647583",
         h3_2: "hanna.kanerva[at]outlook.com",
-        h2_4: "Fysiotraining",
-        p_1: "1 x 60min 65€",
-        p_2: "3 x 60min 180€",
-        p_3: "Personligt träningsprogram 65€ Till Fysiotraining paketets kunder Inbody på Aquarius 30€."
+        h2_4: "Personlig Träning",
+        p_1: "1 x 60min 80€ (inkl. 24% moms)",
+        p_2: "3 x 60min 220€ (inkl. 24% moms)",
+        p_3: "Pt paketets kunder Inbody på Aquarius"
     }
 }
